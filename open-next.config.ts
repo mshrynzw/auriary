76ec4e9516d6c860b1b3ba79e68b90ab132c6c5e
mshrynzw <iq87io25@gmail.com>
2025-11-17 08:13:53 +0900

    fix(all): Cloudflareに対応した

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,9 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+/** @type {import("open-next").OpenNextConfig} */
+const config = {
+  outputMode: 'export',
+  cloudflare: {
+    pages: true,
+  },
+};
 
-export default defineCloudflareConfig({
-  // Uncomment to enable R2 cache,
-  // It should be imported as:
-  // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
-  // See https://opennext.js.org/cloudflare/caching for more details
-  // incrementalCache: r2IncrementalCache,
-});
+export default config;
